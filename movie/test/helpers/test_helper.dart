66a5524aee270648c21b2a2c7fc341029239ae30
Angga Nugraha <Angga>
2022-09-28 14:45:35 +0700

    Merubah state management provider dengan bloc pada Modul movie

--- movie/test/helpers/test_helper.dart
+++ movie/test/helpers/test_helper.dart
@@ -1,6 +1,6 @@
 import 'package:core/core.dart';
-import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
 import 'package:movie/data/datasource/movie_local_data_source.dart';
 import 'package:movie/data/datasource/movie_remote_data_source.dart';
 import 'package:movie/domain/repositories/movie_repository.dart';
