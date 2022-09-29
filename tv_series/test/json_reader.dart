15c746b370bbff88c710254134b05b3c29a2f494
Angga Nugraha <Angga>
2022-09-29 15:19:07 +0700

    update file json_reader.dart

--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -2,8 +2,13 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
+
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+
+  if (dir.endsWith('tv_series')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  return File('$dir/tv_series/test/$name').readAsStringSync();
 }
