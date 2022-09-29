949394074ff3a142b18305fe9a589f439cf69f94
Angga Nugraha <Angga>
2022-09-29 15:01:43 +0700

    init commit

--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -2,13 +2,8 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  // if (dir.endsWith('/test')) {
-  //   dir = dir.replaceAll('/test', '');
-  // }
-
-  if (dir.endsWith('tv_series')) {
-    return File('$dir/test/$name').readAsStringSync();
+  if (dir.endsWith('/test')) {
+    dir = dir.replaceAll('/test', '');
   }
-
-  return File('$dir/tv_series/$name').readAsStringSync();
+  return File('$dir/test/$name').readAsStringSync();
 }
