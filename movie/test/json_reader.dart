8ef2946386aa71f7aad0467a5ec3c416d571cd7f
Angga Nugraha <76716099+Angga-Nugraha@users.noreply.github.com>
2022-09-29 15:05:22 +0700

    Update json_reader.dart

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -4,7 +4,7 @@ String readJson(String name) {
   var dir = Directory.current.path;
 
   if (dir.endsWith('movie')) {
-    return File('$dir/test/$name').readAsStringSync();
+    return File('$dir/movie/$name').readAsStringSync();
   }
-  return File('$dir/movie/$name').readAsStringSync();
+  return File('$dir/test/$name').readAsStringSync();
 }
